5bffecf9dcb6355ebc10a447ebf610b3cb13d957
Wangzy <wangziyuan455@Gmail.com>
2025-05-02 10:34:26 +0800

    feat: init

---
+++ src/main/java/com/wzy/springaiagent/SpringAiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.wzy.springaiagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAiAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wzy/springaiagent/SpringAiAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wzy.springaiagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
